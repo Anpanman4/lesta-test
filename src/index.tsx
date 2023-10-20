dbf5f752c265a9c0b14142162ae1c933957a6cb9
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-10-20 21:20:51 +0300

    refactor: delete useless file

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./vendor/normalize.css";
 import App from "./components/App/App";
-import { Provider } from "react-redux";
-import { store } from "./store/index";
 
 const client = new ApolloClient({
   uri: "https://vortex.korabli.su/api/graphql/glossary/",
@@ -14,11 +12,9 @@ const client = new ApolloClient({
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <Provider store={store}>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ApolloProvider>
-  </Provider>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 );
--- src/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import cashReducer from "./cashReducer";
-
-export const rootReducer = combineReducers({
-  cash: cashReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
